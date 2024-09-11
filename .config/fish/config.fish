baad43fbb26d145e35fbdfe00d8c5338e3b74c8b
Jon <jon@hacky.pro>
2024-09-11 09:42:39 -0500

    updating

--- .config/fish/config.fish
+++
@@ -1,8 +0,0 @@
-. ~/.config/fish/aliases.fish
-
-# Turn off the greeting
-set -U fish_greeting ""
-
-# Starship 
-starship init fish | source
-alias config='/usr/bin/git --git-dir=/home/hacky/.cfg/ --work-tree=/home/hacky'
